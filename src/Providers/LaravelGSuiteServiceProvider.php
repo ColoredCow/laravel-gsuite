dd311f59fe07705072d826804a6e82ab8173f117
Vaibhav Rathore <vaibhav@coloredcow.in>
2018-07-30 15:43:12 +0530

    migrations

--- src/Providers/LaravelGSuiteServiceProvider.php
+++ src/Providers/LaravelGSuiteServiceProvider.php
@@ -13,7 +13,7 @@ class LaravelGSuiteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->publishMigrations();
     }
 
     /**
@@ -24,5 +24,16 @@ class LaravelGSuiteServiceProvider extends ServiceProvider
     public function register()
     {
         $this->loadRoutesFrom(__DIR__ . '../../routes/web.php');
+    }
+
+    protected function publishMigrations()
+    {
+        if (!class_exists('CreateUsersTable')) {
+            $timestamp = date('Y_m_d_His', time());
+
+            $this->publishes([
+                __DIR__ . '/../database/migrations/create_users_table.php.stub' => $this->app->databasePath() . "/migrations/{$timestamp}_create_users_table.php",
+            ], 'migrations');
+        }
     }
 }
