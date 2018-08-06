f38f7a1487b722e208a33a179c273bb11436f2bf
panakj-ag <pankaj@coloredcow.in>
2018-08-06 19:07:04 +0530

    test cases

--- tests/ServiceTest.php
+++ tests/ServiceTest.php
@@ -2,11 +2,15 @@
 
 namespace ColoredCow\LaravelGSuite\Test;
 
+use ColoredCow\LaravelGSuite\Services\Service;
+use Illuminate\Contracts\Container\BindingResolutionException;
+
 class ServiceTest extends TestCase
 {
 	/** @test */
-	public function exampleTest()
+	public function a_service_can_not_be_initialize_alone()
 	{
-		$this->assertTrue(true);
+        $this->expectException(BindingResolutionException::class);
+        $serviceClass = app(Service::class);
 	}
 }
